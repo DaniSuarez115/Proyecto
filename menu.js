fb258d608b9335eea614ac604283e822cbf8f76a
Gustavo Baldizón Duarte <106478888+Tavinho8@users.noreply.github.com>
2023-05-12 14:06:59 -0600

    cambio repositorio 12/5

---
+++ menu.js
@@ -0,0 +1,35 @@
+contenidoMenu.innerHTML += `
+<ul class="nav nav-tabs" id="navId" role="tablist">
+<li class="nav-item">
+    <a href="#tab1Id" class="nav-link active" data-bs-toggle="tab" aria-current="page">Home</a>
+</li>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Gestionar</a>
+    <div class="dropdown-menu">
+        <a class="dropdown-item" href="#tab2Id">Estudiante</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#tab3Id">Profesor</a>
+        
+    </div>
+</li>
+<li class="nav-item" role="presentation">
+    <a href="#tab5Id" class="nav-link" data-bs-toggle="tab">Grupo</a>
+</li>
+<li class="nav-item" role="presentation">
+    <a href="#" class="nav-link" data-bs-toggle="tab">Curso</a>
+</li>
+</ul>
+<div class="tab-content" id="myTabContent">
+<div class="tab-pane fade show active" id="tab1Id" role="tabpanel"></div>
+<div class="tab-pane fade" id="tab2Id" role="tabpanel"></div>
+<div class="tab-pane fade" id="tab3Id" role="tabpanel"></div>
+<div class="tab-pane fade" id="tab4Id" role="tabpanel"></div>
+<div class="tab-pane fade" id="tab5Id" role="tabpanel"></div>
+</div>
+
+<!-- (Optional) - Place this js code after initializing bootstrap.min.js or bootstrap.bundle.min.js -->
+<script>
+var triggerEl = document.querySelector('#navId a')
+bootstrap.Tab.getInstance(triggerEl).show() // Select tab by name
+</script>
+`;
